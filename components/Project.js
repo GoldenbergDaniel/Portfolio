ce6c8c7104b3579e2fa36c29ad779cbc09a04ac8
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-07-15 13:36:04 -0400

    Removed glow

---
+++ components/Project.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styles from "../styles/Project.module.scss"
+
+var Project = (props) => {
+  return (
+    <div id={styles.main}>
+      <div id={styles.title} className={styles.container}>
+        <h1>{props.title}</h1>
+      </div>
+      <div id={styles.photo} className={styles.container}>
+        <img src={props.image} alt="Screenshot of project"/>
+      </div>
+      <div id={styles.technologies} className={styles.container}>
+        <ul>
+          {props.technologies.map(technology => <li>{technology}</li>)}
+        </ul>
+      </div>
+      <div id={styles.desc} className={styles.container}>
+        <p>{props.desc}</p>
+      </div>
+      <div id={styles.link} className={styles.container}>
+        <a href={props.link} target="_blank">Code</a>
+      </div>
+    </div>
+  )
+}
+
+export default Project
--- components/pageContent/MyProjects.js
+++ components/pageContent/MyProjects.js
@@ -1,12 +1,21 @@
 import styles from "../../styles/pageStyles/MyProjects.module.scss"
 
 import Heading from "../Heading"
+import Project from "../Project"
 
 var MyProject = () => {
   return (
     <div id={styles.main}>
       <div id="left"></div>
       <Heading title="My Projects"/>
+      <div id={styles.projects}>
+        <Project 
+          title="Formula Frenzy" 
+          desc="Formula Frenzy is a game we made" 
+          technologies={["python", "js", "react.js"]} 
+          link="https://github.com/GoldenbergDaniel/FormulaFrenzy"
+        />
+      </div>
     </div>
   )
 }
